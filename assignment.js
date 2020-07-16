4c60a0908aa94eac580b5db6b79c2d84b5b35989
paul1680 <67039598+paul1680@users.noreply.github.com>
2020-07-17 02:31:38 +0600

    third

--- assignment.js
+++ assignment.js
@@ -15,5 +15,41 @@ function woodCalculator(chair, table, bed){
 
 }
 
-var wood = woodCalculator(1, 1, 1);
+var wood = woodCalculator(14, 5, 7);
 console.log(wood);
+
+
+function brickCalculator(floor){
+    if (floor <= 10){
+        var firstTenFloor = 15* floor;
+        var result = firstTenFloor;
+    }
+    else if(floor <=20){
+        var secondTenFloor = 12 * floor;
+        var result = secondTenFloor + 30;    
+    }
+    else{
+        var lastFloor = floor * 10;
+        var result = lastFloor + 50 + 20;
+    }
+    var totalBrick = 1000 * result;
+    return totalBrick;
+        
+
+}
+var numberOfBricks = brickCalculator(11);
+console.log(numberOfBricks);
+
+
+function tinyFriend(friend){
+    smallName = friendName[0];
+    for(var i = 0; i<friendName.length; i++){
+        var element = friendName[i];
+        if (element < smallName){
+            smallName = element;
+        }
+    }
+    return smallName;
+}
+var now = tinyFriend([5, 6, 8, 2, 1]);
+console.log(now);
