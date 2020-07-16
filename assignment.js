c76c7efce7c397cc96a8bd199a4842210370a6bf
paul1680 <67039598+paul1680@users.noreply.github.com>
2020-07-16 23:56:15 +0600

    2nd

--- assignment.js
+++ assignment.js
@@ -4,3 +4,16 @@ function feetToMile(feet){
 }
 var inputFeet = feetToMile(5281);
 console.log(inputFeet);
+
+
+function woodCalculator(chair, table, bed){
+    var numberOfChair = 1 * chair;
+    var numberOfTable = 3 * table;
+    var numberOfBed = 5 * bed;
+    var total = numberOfChair + numberOfTable + numberOfBed;
+    return total;
+
+}
+
+var wood = woodCalculator(1, 1, 1);
+console.log(wood);
